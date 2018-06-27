fd3cc103fb1f384cf70bf544a7d606ce87f33621
Your name <your.example.com>
2018-06-27 19:18:01 +0200

    eliminacioon de comentarion

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,12 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IHabitacion } from './../../interfaces/IHabitacion';
-/*
-  Generated class for the ApiProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ApiProvider {
 
